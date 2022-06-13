c61c845591ea384db1e7e4f695961a7a2d5be6d5
davis <davisochieng4@gmail.com>
2022-06-13 05:32:30 -0400

    added the Header component and Links to shop and contact page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes, useParams, Link, Outlet } from 'react-router-dom';
 
 import { HomePage } from './pages/homepage/homepage';
 import { ShopPage } from './pages/shop/shop';
+import Header from './components/header/header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -3,7 +3,6 @@ import React from 'react';
 import './collection-item.style.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
-  console.log(imageUrl);
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }}>
