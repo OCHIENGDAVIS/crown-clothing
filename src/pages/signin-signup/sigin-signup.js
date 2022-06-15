a400a9544e6d6eab629fba93097d3611b268e63d
davis <davisochieng4@gmail.com>
2022-06-15 06:00:30 -0400

    added firebase utils, ability to store users into the firestore database

--- src/pages/signin-signup/sigin-signup.js
+++ src/pages/signin-signup/sigin-signup.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './sigin-signup.style.style.scss';
 import Signin from '../../components/sigin/signin';
+import SignUp from '../../components/signup/signup';
 
 const SignUpIn = () => {
   return (
-    <div className="sign-in-sign-up">
+    <div className="sign-in-and-sign-up">
       <Signin />
+      <SignUp />
     </div>
   );
 };
